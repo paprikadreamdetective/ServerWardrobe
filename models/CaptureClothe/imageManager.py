27cccf81acfcbd10bdffea24f33fb1f28c4b6c8f
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-05-19 20:45:44 -0600

    delete unnecessesary files

--- models/CaptureClothe/imageManager.py
+++ models/CaptureClothe/imageManager.py
@@ -2,10 +2,13 @@ from serviceImageProcessing.ProxyImageProcessing import ProxyImageProcessing
 from serviceImageProcessing.PI4Connector import PI4Connector
 
 def sendPictureToPI(file_path, picture):
+    return ProxyImageProcessing(PI4Connector()).send(file_path, picture)
+    '''
     if ProxyImageProcessing(PI4Connector()).send(file_path, picture):
         return True
     else:
         return False
+    '''
     
 #def sendImage(image):
 #    ProxyImageProcessing(PI4Connector()).send(image)
