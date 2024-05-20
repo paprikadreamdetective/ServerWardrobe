2a604e51a9b98517f5efffe098d2c3d0a0633531
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-05-20 11:58:18 -0600

    migrate code of controllers to facade

--- facadeController.py
+++ facadeController.py
@@ -1,13 +1,12 @@
 #from models.
 # ControladorAuth
+import os
 from app import app 
 from flask import request, jsonify
 
-from services.userServices.UserManager import user_auth 
-
-#def index():
-#    return {'status': 'OK',
-#            'localhost:5000/ctrl/login':'user login'}
+from models.Authenticate.authManager import user_auth
+from models.CaptureClothe.imageManager import sendPictureToPI
+from models.CreateOutfit.cliente import create_outfit
 
 
 @app.route('/login', methods=['POST'])
@@ -23,3 +22,26 @@ def login():
         return jsonify({'success': False, 'message': 'Invalid credentials'})
 
 
+@app.route('/upload_picture', methods=['POST'])
+def upload_picture():
+    picture = request.files['file']
+    if picture:
+        file_path = os.path.join('uploads', picture.filename)
+        picture.save(file_path)
+        if sendPictureToPI(file_path, picture):
+            return jsonify({"message": "Image received and forwarded successfully!", "file_path": file_path})
+        else:
+            return jsonify({"message": "Image received but failed to forward", "error": "ERROR"}), 500
+    return jsonify({"message": "No file received"}), 400
+ 
+@app.route('/create_outfit', methods=['POST'])
+def createOutfit():
+    create_outfit()
+
+@app.route('/get_clothe', methods=['GET'])
+def get_clothe():
+    pass
+
+@app.route('/register', methods=['POST'])
+def register():
+    pass
--- models/CreateOutfit/cliente.py
+++ models/CreateOutfit/cliente.py
@@ -1,7 +1,8 @@
 from master import Master_ConjuntoRopa
 
 # Instanciar la clase Master_ConjuntoRopa
-conjunto = Master_ConjuntoRopa(1, 1, 1, 1)
+def create_outfit():
+    conjunto = Master_ConjuntoRopa(1, 1, 1, 1)
 
-# Ejecutar la creación del conjunto de ropa
-conjunto.ejecucion_CrearConjunto()
+    # Ejecutar la creación del conjunto de ropa
+    conjunto.ejecucion_CrearConjunto()
