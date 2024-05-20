293e79640a6362a556447063d73e3402435e7512
Jorge Juarez <jorgeang33@gmail.com>
2024-05-20 12:32:58 -0600

    add factories controllers 🏭

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
+from controllers.ImageController import *
+from controllers.AuthController import *
 from flask import Flask
 from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)  # flask app object
     CORS(app)
     return app
 
+
 app = create_app()  # Creating the app
-from controllers.AuthController import * 
-from controllers.ImageController import * 
 
 if __name__ == '__main__':  # Running the app
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
     app.run(host='127.0.0.1', port=5000, debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
