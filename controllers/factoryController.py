267c3420f27f8f31d185f696a24b6c7b1fb60c6a
Jorge Juarez <jorgeang33@gmail.com>
2024-05-20 12:40:35 -0600

    Refactor image processing and detection code ⛰️

--- controllers/factoryController.py
+++ controllers/factoryController.py
@@ -1,51 +1,9 @@
-from ultralytics import YOLO
-from rembg import remove
-
 from objectCreation.FactoryMethodClothes.CreatorButtom import CreatorButtom
 from objectCreation.FactoryMethodClothes.CreatorTop import CreatorTop
 from objectCreation.FactoryMethodClothes.CreatorShoes import CreatorShoes
 from objectCreation.FactoryMethodClothes.CreatorAccessory import CreatorAccessory
 from objectCreation.FactoryMethodClothes.Creator import client_factory
-
-
-def remove_backgroud_image(input_image, output_image):
-    if input_image.endswith(('.png', 'jpg', 'jpeg')):
-        with open(input_image, 'rb') as inp, open(output_image, 'wb') as outp:
-            backgroud_output = remove(inp.read())
-            outp.write(backgroud_output)
-    else:
-        print("The file is not valid")
-
-
-def do_detection(image) -> str:
-    """
-    Detections :
-        - dress
-        - hat
-        - longsleeve
-        - outwear
-        - pants
-        - shirt
-        - shoes
-        - shorts
-        - skirt
-        - t-shirt
-    """
-    model = YOLO('model/clothes_classification.pt')
-    results = model(image, verbose=False)
-    for r in results:
-        probs = r.probs
-        names = r.names
-        clothe = probs.top1
-
-    clothing_detected = names[clothe]
-    return clothing_detected
-
-
-def process_image(in_image, out_image):
-    remove_backgroud_image(in_image, out_image)
-    clothe = do_detection(out_image)
-    return clothe
+from model.imageProcessing.process import process_image
 
 
 def choose_creator(clothe_class):
---
+++ model/imageProcessing/process.py
@@ -0,0 +1,42 @@
+from ultralytics import YOLO
+from rembg import remove
+
+
+def remove_backgroud_image(input_image, output_image):
+    if input_image.endswith(('.png', 'jpg', 'jpeg')):
+        with open(input_image, 'rb') as inp, open(output_image, 'wb') as outp:
+            backgroud_output = remove(inp.read())
+            outp.write(backgroud_output)
+    else:
+        print("The file is not valid")
+
+
+def do_detection(image) -> str:
+    """
+    Detections :
+        - dress
+        - hat
+        - longsleeve
+        - outwear
+        - pants
+        - shirt
+        - shoes
+        - shorts
+        - skirt
+        - t-shirt
+    """
+    model = YOLO('model/clothes_classification.pt')
+    results = model(image, verbose=False)
+    for r in results:
+        probs = r.probs
+        names = r.names
+        clothe = probs.top1
+
+    clothing_detected = names[clothe]
+    return clothing_detected
+
+
+def process_image(in_image, out_image):
+    remove_backgroud_image(in_image, out_image)
+    clothe = do_detection(out_image)
+    return clothe
