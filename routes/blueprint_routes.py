79951ab565395ca6048b53996ac4524d01f2f981
Jorge Juarez <jorgeang33@gmail.com>
2024-05-20 12:11:00 -0600

    clean repo 🧹

--- routes/blueprint_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controllers.facadeController import index, login
-
-blueprint = Blueprint('blueprint', __name__)
-
-blueprint.route('/', methods=['GET'])(index)
-blueprint.route('/login', methods=['POST'])(login)
-# blueprint.route('/register', methods=['POST'])(insert)
--- services/userServices/UserManager.py
+++
@@ -1,8 +0,0 @@
-from .UserCrud import UserCrud
-from .ProxyUser import ProxyUser
-
-
-def user_auth(username: str, password: str):
-    return ProxyUser(UserCrud()).auth(username, password)
-    #proxy = ProxyUser(UserCrud())
-    #proxy.auth(username, password)
