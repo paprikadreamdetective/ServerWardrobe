9b0c8fb03ba8cb5203fe50ce91b88e089b654ce8
Jorge Juarez <jorgeang33@gmail.com>
2024-05-22 12:14:36 -0600

    Remove commented code for generating random outfits

--- objectCreation/AbstractFactoryOutfits/AbstractOutfitFactory.py
+++ objectCreation/AbstractFactoryOutfits/AbstractOutfitFactory.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-import random
-from pprint import pprint
 from .Outfit import Outfit
+
+
 class AbstractOutfitFactory(ABC):
     @abstractmethod
     def create_casual_outfit(self) -> 'Outfit':
@@ -14,20 +14,3 @@ class AbstractOutfitFactory(ABC):
     @abstractmethod
     def create_random_outfit(self) -> dict:
         pass
-        # """
-        # Genera un outfit aleatorio combinando prendas de manera aleatoria.
-        # """
-
-        # outfit = {"top": {}, "buttom": {}, "shoes": {}, "accessory": {}}
-        # ramdom_outfit = []
-
-        # for types in clothes_list:
-        #     ramdom_clothe = random.choice(list(types))
-        #     ramdom_outfit.append(ramdom_clothe)
-
-        # outfit['top'].update({ramdom_outfit[0]: clothes_list[0][ramdom_outfit[0]]})
-        # outfit['buttom'].update({ramdom_outfit[1]: clothes_list[1][ramdom_outfit[1]]})
-        # outfit['shoes'].update({ramdom_outfit[2]: clothes_list[2][ramdom_outfit[2]]})
-        # outfit['accessory'].update({ramdom_outfit[3]: clothes_list[3][ramdom_outfit[3]]})
-
-        # return str(outfit)
